be53bf7a20a5011113d8aa40c4b1885458915c26
starrpatty28 <starrpatty28@gmail.com>
2017-03-15 11:47:07 -0700

    add 'StatePicker' and set to be hidden unless Clkd. Also, add two IBOutlets 'statePickerBtn and statePicker'

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePicketBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: Any) {
+    }
 
 }
 
